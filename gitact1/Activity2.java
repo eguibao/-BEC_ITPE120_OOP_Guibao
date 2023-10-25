fc9450b1d9ac1c26827ed19762555bf9067c0a69
eguibao <148948685+eguibao@users.noreply.github.com>
2023-10-25 16:10:26 +0800

    Add files via upload
    updated

---
+++ gitact1/Activity2.java
@@ -0,0 +1,14 @@
+public class Activity2 {
+    public static double calculateAverage(double... numbers) {
+        if (numbers.length == 0) {
+            return 0.0;
+        }
+
+        double sum = 0.0;
+        for (double number : numbers) {
+            sum += number;
+        }
+
+        return sum / numbers.length;
+    }
+}
---
+++ gitact1/objact2.java
@@ -0,0 +1,10 @@
+public class objact2{
+    public static void main(String[] args) {
+        double[] values = { 10.5, 7.2, 3.8, 12.1, 5.4 };
+
+        // Use the static method to calculate the average
+        double average = Activity2.calculateAverage(values);
+
+        System.out.println("Average: " + average);
+    }
+}
